7fad04466c2089928ea7a7d162eb7469a8a4f781
Mathilde Laussel <mlaussel@z3r2p6.42lyon.fr>
2025-05-14 15:38:09 +0200

    try compare map

--- src/laussel/ft_compare_map.c
+++ src/laussel/ft_compare_map.c
@@ -6,7 +6,7 @@
 /*   By: mlaussel <mlaussel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 13:06:37 by mlaussel          #+#    #+#             */
-/*   Updated: 2025/05/14 14:14:01 by mlaussel         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:37:46 by mlaussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,61 +30,30 @@ static int	ft_size_map(char **map)
 	return (len);
 }
 
-static int	ft_is_backslash_n(char *str)
+static int	ft_compare_map(t_gnl *gnl, char **map)
 {
 	int	i;
-
-	i = 0;
-	while (ft_is_white_space(str[i]))
-		i++;
-	if (str[i] == '\n')
-		return (0);
-	return (1);
-}
-
-static int	ft_compare_map(char	**map1, char **map2)
-{
-	int	i;
-	int	j;
+	t_gnl	*list;
 	int	len;
 
-	i = ft_size_map(map1);
-	j = ft_size_map(map2);
-	while (ft_is_backslash_n(map2[j]))
-		j--;
+	i = ft_size_map(map);
+	while (list->next)
+		list = list->next;
+	while (list->str == NULL)
+		list = list->prev;
 	while (i > 6)
 	{
-		len = ft_strlen(map1[i]);
-		if (ft_strncmp(map1[i], map2[j], len) != 0)
+		len = ft_strlen(map[i]);
+		if (ft_strncmp(map[i], list, len) != 0)
 		{
 			debug_put_int_d1("DEBUG--> map ok : ", 0, 31, 310);
 			return (0);
 		}
 		i--;
-		j--;
+		list = list->prev;
 	}
 	debug_put_int_d1("DEBUG--> map ok : ", 1, 31, 310);
 	return (1);
-}
-
-static void	ft_replace_n(char *map)
-{
-	int	i;
-
-	i = 0;
-	while (map[i])
-	{
-		if (map[i] == '\n' && map[i + 1] && map[i + 1] == '\n')
-		{
-			i++;
-			map[i] = -42;
-
-			// if (map[i + 1])
-			// 	i++;
-		}
-		i++;
-	}
-	debug_put_str("replace - 42 : ",map, 31, 310);
 }
 
 int	ft_split_and_replace(t_data *data)
@@ -96,12 +65,7 @@ int	ft_split_and_replace(t_data *data)
 	if (map_1 == NULL)
 		return (-1);
 	debug_put_tab("split \\n :", map_1, 31, 310);
-	ft_replace_n(data->maps.file_in_a_line);
-	map_2 = ft_split(data->maps.file_in_a_line, '\n');
-	if (map_2 == NULL)
-		return (-1);
-	debug_put_tab("split -42 :", map_1, 31, 310);
-	ft_compare_map(map_1, map_2);
+	ft_compare_map(data->maps.gnl, map_1);
 	return (1);
 }
 
