bd71cc171e1ff17909f886731dd9910361cfc100
Mathilde Laussel <mlaussel@z4r2p2.42lyon.fr>
2025-06-18 16:41:35 +0200

    colision ok

--- src/utils/event/ft_wasd.c
+++ src/utils/event/ft_wasd.c
@@ -6,7 +6,7 @@
 /*   By: mlaussel <mlaussel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:06:29 by mlaussel          #+#    #+#             */
-/*   Updated: 2025/06/18 16:34:36 by mlaussel         ###   ########.fr       */
+/*   Updated: 2025/06/18 16:40:23 by mlaussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,61 @@
 
 static int	ft_w(t_player *player)
 {
+	double	pos_x;
+	double	pos_y;
+
 	debug_put_str("[w] is pressed", NULL, 2, 21);
-	if (ft_check_pos_colision(player, player->pos_x, player->pos_y - MS))
+	pos_x = player->pos_x + sin(player->angle) * MS;
+	pos_y = player->pos_y - cos(player->angle) * MS;
+	if (ft_check_pos_colision(player, pos_x, pos_y))
 		return (-1);
-	player->pos_x = player->pos_x + sin(player->angle) * MS;
-	player->pos_y = player->pos_y - cos(player->angle) * MS;
+	player->pos_x = pos_x;
+	player->pos_y = pos_y;
 	return (1);
 }
 
 static int	ft_a(t_player *player)
 {
+	double	pos_x;
+	double	pos_y;
+
 	debug_put_str("[a] is pressed", NULL, 2, 21);
-	if (ft_check_pos_colision(player, player->pos_x - MS, player->pos_y))
+	pos_x = player->pos_x - cos(player->angle) * MS;
+	pos_y = player->pos_y - sin(player->angle) * MS;
+	if (ft_check_pos_colision(player, pos_x, pos_y))
 		return (-1);
-	player->pos_x = player->pos_x - cos(player->angle) * MS;
-	player->pos_y = player->pos_y - sin(player->angle) * MS;
+	player->pos_x = pos_x;
+	player->pos_y = pos_y;
 	return (1);
 }
 
 static int	ft_s(t_player *player)
 {
+	double	pos_x;
+	double	pos_y;
+
 	debug_put_str("[s] is pressed", NULL, 2, 21);
-	if (ft_check_pos_colision(player, player->pos_x, player->pos_y + MS))
+	pos_x = player->pos_x - sin(player->angle) * MS;
+	pos_y = player->pos_y + cos(player->angle) * MS;
+	if (ft_check_pos_colision(player, pos_x, pos_y ))
 		return (-1);
-	player->pos_x = player->pos_x - sin(player->angle) * MS;
-	player->pos_y = player->pos_y + cos(player->angle) * MS;
+	player->pos_x = pos_x;
+	player->pos_y = pos_y;
 	return (1);
 }
 
 static int	ft_d(t_player *player)
 {
+	double	pos_x;
+	double	pos_y;
+
 	debug_put_str("[d] is pressed", NULL, 2, 21);
-	if (ft_check_pos_colision(player, player->pos_x + MS, player->pos_y))
+	pos_x = player->pos_x + cos(player->angle) * MS;
+	pos_y = player->pos_y + sin(player->angle) * MS;
+	if (ft_check_pos_colision(player, pos_x, pos_y))
 		return (-1);
-	player->pos_x = player->pos_x + cos(player->angle) * MS;
-	player->pos_y = player->pos_y + sin(player->angle) * MS;
+	player->pos_x = pos_x;
+	player->pos_y = pos_y;
 	return (1);
 }
 
