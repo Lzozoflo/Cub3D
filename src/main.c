506a9a5d3786141bf1564486834b054dd230c01d
Mathilde Laussel <mlaussel@z4r2p2.42lyon.fr>
2025-06-23 13:33:21 +0200

    norme

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mlaussel <mlaussel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:58:30 by fcretin           #+#    #+#             */
-/*   Updated: 2025/06/23 12:43:06 by mlaussel         ###   ########.fr       */
+/*   Updated: 2025/06/23 13:33:06 by mlaussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,7 @@ int	main(int ac, char **av, char **env)
 	}
 	t_minimap_set_color(&data.exec, &data.exec.mini);
 	if (ft_refresh_view(&data) == -1)
-	{
-		ft_free_cardi(&data.exec);
-		ft_free_radius(&data.exec.radius, data.win_scale);
 		return (1);
-	}
 	mlx_loop_hook(data.mlx, ft_handle_keys, &data);
 	mlx_hook(data.win, KeyPress, KeyPressMask, ft_key_press, &data);
 	mlx_hook(data.win, KeyRelease, KeyReleaseMask, ft_key_release, &data);
